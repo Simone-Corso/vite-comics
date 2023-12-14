7e38097077957f8c5382dbc441760502b2899785
Simone-Corso <scorso26@gmail.com>
2023-12-14 16:16:21 +0100

    ho creato un componente per il menu comics

---
+++ src/components/AppMenu.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'MenuComics',
+
+    data() {
+        return {
+            MenuNav: []
+        }
+    },
+    
+}
+</script>
+<style lang="">
+    
+</style>

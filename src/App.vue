78ab562aff798e7ee1f5bb2cb4bcca44cd77d9cd
Simone-Corso <scorso26@gmail.com>
2023-12-14 15:13:26 +0100

    ho aggiunto dei componenti separati nel app vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template lang="">
   <div>
-      
+  </AppHeader>
+  </AppMain>
+  </AppFooter>
   </div>
 </template>
 <script>
+
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
 export default {
+    name: 'App',
+  components: {
+    AppHeader,
+    AppFooter,
+    AppMain
+  }
   
 }
 </script>

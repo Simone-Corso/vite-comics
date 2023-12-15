0388a1df68d521b8064ee0d93a6976631f026017
Simone-Corso <scorso26@gmail.com>
2023-12-15 08:44:34 +0100

    ho inserito immagine bg footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,50 @@
 <template lang="">
-    <div>
-        <h1>magnifico</h1>
-    </div>
+    <footer>
+       <div class="container-img">
+        <img src="../assets/img/footer-bg.jpg" alt="">
+       </div>
+
+       <ul>
+        <h2>
+            DC COMICS
+        </h2>
+        <li></li>
+       </ul>
+
+       <ul>
+        <h2>
+
+        </h2>
+        <li></li>
+       </ul>
+
+       <ul>
+        <li></li>
+       </ul>
+
+       <ul>
+        <li></li>
+       </ul>
+    </footer>
 </template>
 <script>
 export default {
     
 }
 </script>
-<style lang="">
+<style lang="scss" scoped>
+
+.container-img{
+    width: 100%;
+    height: 450px;
+
+}
+
+img{
+    width: 100%;
+    height: 450px;
+}
+   
+
     
 </style>

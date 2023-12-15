9e65cf809b113bec9085db959ad4941c4170b814
Simone-Corso <scorso26@gmail.com>
2023-12-15 08:57:31 +0100

    inserito la lista

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,30 +1,55 @@
 <template lang="">
     <footer>
        <div class="container-img">
-        <img src="../assets/img/footer-bg.jpg" alt="">
-       </div>
-
        <ul>
         <h2>
             DC COMICS
         </h2>
-        <li></li>
+        <li>Characters</li>
+        <li>Comics</li>
+        <li>Movies</li>
+        <li>TV</li>
+        <li>Games</li>
+        <li>Videos</li>
+        <li>News</li>
        </ul>
 
        <ul>
         <h2>
-
+            SHOP
         </h2>
-        <li></li>
+        <li>Shop DC</li>
+        <li>Shop DC collectibles</li>
        </ul>
 
        <ul>
-        <li></li>
+        <h2>
+            DC
+        </h2>
+        <li>Terms Of Use</li>
+        <li>Privacy policy (New)</li>
+        <li>Ad Choices</li>
+        <li>Advertsing</li>
+        <li>Jobs</li>
+        <li>Subscriptions</li>
+        <li>Talent Workshops</li>
+        <li>CPSC Certificates</li>
+        <li>Ratings</li>
+        <li>Shop Help</li>
+        <li>Contact Us</li>
        </ul>
 
        <ul>
-        <li></li>
-       </ul>
+        <h2>
+            SITES
+        </h2>
+        <li>DC</li>
+        <li>MAD Magazine</li>
+        <li>DC Kids</li>
+        <li>DC Universe</li>
+        <li>DC Power Visa</li>  
+    </ul>
+       </div>
     </footer>
 </template>
 <script>
@@ -37,6 +62,12 @@ export default {
 .container-img{
     width: 100%;
     height: 450px;
+    background-image: url(../assets/img/footer-bg.jpg);
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-content: flex-start;
 
 }
 
@@ -44,7 +75,16 @@ img{
     width: 100%;
     height: 450px;
 }
-   
+
+ul{
+    color: #797979;
+    list-style-type: none;
+}
+
+h2{
+    color: white;
+    font-size: 20px;
+}
 
     
 </style>
